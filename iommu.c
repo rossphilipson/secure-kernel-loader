4cbe12c15765b4fa1f522d945a900187d1823e74
Ross Philipson <ross.philipson@oracle.com>
2022-07-07 09:36:50 -0400

    Debug code and IOMMU fix for reference
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- iommu.c
+++ iommu.c
@@ -19,6 +19,7 @@
 #include <types.h>
 #include <pci.h>
 #include <iommu.h>
+#include <printk.h>
 
 iommu_dte_t device_table[2 * PAGE_SIZE / sizeof(iommu_dte_t)] __page_data = {
     [0 ... ARRAY_SIZE(device_table) - 1 ] = {
@@ -27,50 +28,6 @@ iommu_dte_t device_table[2 * PAGE_SIZE / sizeof(iommu_dte_t)] __page_data = {
 };
 iommu_command_t command_buf[2] __aligned(sizeof(iommu_command_t));
 char event_log[PAGE_SIZE] __page_data;
-
-#ifdef DEBUG
-static void print_char(char c)
-{
-	while ( !(inb(0x3f8 + 5) & 0x20) )
-		;
-
-	outb(c, 0x3f8);
-}
-
-static void print(char * txt) {
-	while (*txt != '\0') {
-		if (*txt == '\n')
-			print_char('\r');
-		print_char(*txt++);
-	}
-}
-
-static void print_u64(u64 p) {
-	char tmp[sizeof(void*)*2 + 5] = "0x";
-	int i;
-
-	for (i=0; i<sizeof(void*); i++) {
-		if ((p & 0xf) >= 10)
-			tmp[sizeof(void*)*2 + 1 - 2*i] = (p & 0xf) + 'a' - 10;
-		else
-			tmp[sizeof(void*)*2 + 1 - 2*i] = (p & 0xf) + '0';
-		p >>= 4;
-		if ((p & 0xf) >= 10)
-			tmp[sizeof(void*)*2 - 2*i] = (p & 0xf) + 'a' - 10;
-		else
-			tmp[sizeof(void*)*2 - 2*i] = (p & 0xf) + '0';
-		p >>= 4;
-	}
-	tmp[sizeof(void*)*2 + 2] = ':';
-	tmp[sizeof(void*)*2 + 3] = ' ';
-	tmp[sizeof(void*)*2 + 4] = '\0';
-	print(tmp);
-}
-#else
-static void print(const char * unused) { }
-static void print_u64(u64 unused) { }
-#endif
-
 
 static u32 _locate(unsigned int bus, unsigned int devfn)
 {
@@ -177,18 +134,24 @@ void disable_memory_protection(void)
 		  MEMPROT_CR, 4, sldev & ~(MEMPROT_EN));
 }
 
-static void send_command(u64 *mmio_base, iommu_command_t cmd)
+static void send_command(u64 *mmio_base, iommu_command_t cmd, u32 *idx)
 {
-    u32 cmd_ptr = mmio_base[IOMMU_MMIO_COMMAND_BUF_TAIL] >> 4;
-    command_buf[cmd_ptr++] = cmd;
+    u32 cmd_ptr = mmio_base[IOMMU_MMIO_COMMAND_BUF_TAIL];
+    print_u64(cmd_ptr);
+    print("cmd_ptr before\n");
+
+    command_buf[(*idx)++] = cmd;
     smp_wmb();
-    mmio_base[IOMMU_MMIO_COMMAND_BUF_TAIL] = (cmd_ptr << 4);
+    cmd_ptr += sizeof(iommu_command_t);
+    mmio_base[IOMMU_MMIO_COMMAND_BUF_TAIL] = cmd_ptr;
+    print_u64(cmd_ptr);
+    print("cmd_ptr after\n");
 }
 
 u32 iommu_load_device_table(u32 cap, volatile u64 *completed)
 {
     u64 *mmio_base;
-    u32 low, hi;
+    u32 low, hi, idx = 0;
     iommu_command_t cmd = {0};
 
     pci_read(0, IOMMU_PCI_BUS,
@@ -207,12 +170,12 @@ u32 iommu_load_device_table(u32 cap, volatile u64 *completed)
 
     mmio_base = _p((u64)hi << 32 | (low & 0xffffc000));
 
-    print("IOMMU MMIO Base Address = ");
-    print_u64((u64)_u(mmio_base));
-    print("\n");
+    //print("IOMMU MMIO Base Address = ");
+    //print_u64((u64)_u(mmio_base));
+    //print("\n");
 
-    print_u64(mmio_base[IOMMU_MMIO_STATUS_REGISTER]);
-    print("IOMMU_MMIO_STATUS_REGISTER\n");
+    //print_u64(mmio_base[IOMMU_MMIO_STATUS_REGISTER]);
+    //print("IOMMU_MMIO_STATUS_REGISTER\n");
 
     /* Disable IOMMU and all its features */
     mmio_base[IOMMU_MMIO_CONTROL_REGISTER] &= ~IOMMU_CR_ENABLE_ALL_MASK;
@@ -221,8 +184,8 @@ u32 iommu_load_device_table(u32 cap, volatile u64 *completed)
     /* Address and size of Device Table (bits 8:0 = 0 -> 4KB; 1 -> 8KB ...) */
     mmio_base[IOMMU_MMIO_DEVICE_TABLE_BA] = (u64)_u(device_table) | 1;
 
-    print_u64(mmio_base[IOMMU_MMIO_DEVICE_TABLE_BA]);
-    print("IOMMU_MMIO_DEVICE_TABLE_BA\n");
+    //print_u64(mmio_base[IOMMU_MMIO_DEVICE_TABLE_BA]);
+    //print("IOMMU_MMIO_DEVICE_TABLE_BA\n");
 
     /*
      * !!! WARNING - HERE BE DRAGONS !!!
@@ -248,16 +211,22 @@ u32 iommu_load_device_table(u32 cap, volatile u64 *completed)
     mmio_base[IOMMU_MMIO_COMMAND_BUF_HEAD] =
         mmio_base[IOMMU_MMIO_COMMAND_BUF_TAIL] = _u(command_buf) & 0xff0;
 
+    print_u64(_u(command_buf));
+    print("command_buf\n");
+
     print_u64(mmio_base[IOMMU_MMIO_COMMAND_BUF_BA]);
     print("IOMMU_MMIO_COMMAND_BUF_BA\n");
+
+    print_u64(mmio_base[IOMMU_MMIO_COMMAND_BUF_HEAD]);
+    print("IOMMU_MMIO_COMMAND_BUF_HEAD\n");
 
     /* Address and size of Event Log, reset head and tail registers */
     mmio_base[IOMMU_MMIO_EVENT_LOG_BA] = (u64)_u(event_log) | (0x8ULL << 56);
     mmio_base[IOMMU_MMIO_EVENT_LOG_HEAD] = 0;
     mmio_base[IOMMU_MMIO_EVENT_LOG_TAIL] = 0;
 
-    print_u64(mmio_base[IOMMU_MMIO_EVENT_LOG_BA]);
-    print("IOMMU_MMIO_EVENT_LOG_BA\n");
+    //print_u64(mmio_base[IOMMU_MMIO_EVENT_LOG_BA]);
+    //print("IOMMU_MMIO_EVENT_LOG_BA\n");
 
     /* Clear EventLogInt set by IOMMU not being able to read command buffer */
     mmio_base[IOMMU_MMIO_STATUS_REGISTER] &= ~2;
@@ -267,15 +236,19 @@ u32 iommu_load_device_table(u32 cap, volatile u64 *completed)
 
     mmio_base[IOMMU_MMIO_CONTROL_REGISTER] |= IOMMU_CR_IommuEn;
 
-    print_u64(mmio_base[IOMMU_MMIO_STATUS_REGISTER]);
-    print("IOMMU_MMIO_STATUS_REGISTER\n");
+    //print_u64(mmio_base[IOMMU_MMIO_STATUS_REGISTER]);
+    //print("IOMMU_MMIO_STATUS_REGISTER\n");
 
+    print_u64(idx);
+    print("idx before\n");
     if ( mmio_base[IOMMU_MMIO_EXTENDED_FEATURE] & IOMMU_EF_IASup )
     {
         print("INVALIDATE_IOMMU_ALL\n");
         cmd.opcode = INVALIDATE_IOMMU_ALL;
-        send_command(mmio_base, cmd);
+        send_command(mmio_base, cmd, &idx);
     } /* TODO: else? */
+    print_u64(idx);
+    print("idx after\n");
 
     print_u64(mmio_base[IOMMU_MMIO_EXTENDED_FEATURE]);
     print("IOMMU_MMIO_EXTENDED_FEATURE\n");
@@ -289,7 +262,7 @@ u32 iommu_load_device_table(u32 cap, volatile u64 *completed)
 
     cmd.opcode = COMPLETION_WAIT;
     cmd.u2 = 0x656e6f64;    /* "done" */
-    send_command(mmio_base, cmd);
+    send_command(mmio_base, cmd, &idx);
 
     print_u64(mmio_base[IOMMU_MMIO_STATUS_REGISTER]);
     print("IOMMU_MMIO_STATUS_REGISTER\n");
